ae8123d71b1d36ca19bfc49a2901c8a2a60c0919
Ngo Sach Nhat <nhatngo11a1@gmail.com>
2020-11-14 12:50:19 +0700

    question 1, 2, 3

---
+++ classification/mira.py
@@ -0,0 +1,105 @@
+# mira.py
+# -------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+# Mira implementation
+import util
+PRINT = True
+
+class MiraClassifier:
+    """
+    Mira classifier.
+
+    Note that the variable 'datum' in this code refers to a counter of features
+    (not to a raw samples.Datum).
+    """
+    def __init__( self, legalLabels, max_iterations):
+        self.legalLabels = legalLabels
+        self.type = "mira"
+        self.automaticTuning = False
+        self.C = 0.001
+        self.legalLabels = legalLabels
+        self.max_iterations = max_iterations
+        self.initializeWeightsToZero()
+
+    def initializeWeightsToZero(self):
+        "Resets the weights of each label to zero vectors"
+        self.weights = {}
+        for label in self.legalLabels:
+            self.weights[label] = util.Counter() # this is the data-structure you should use
+
+    def train(self, trainingData, trainingLabels, validationData, validationLabels):
+        "Outside shell to call your method. Do not modify this method."
+
+        self.features = trainingData[0].keys() # this could be useful for your code later...
+
+        if (self.automaticTuning):
+            Cgrid = [0.002, 0.004, 0.008]
+        else:
+            Cgrid = [self.C]
+
+        return self.trainAndTune(trainingData, trainingLabels, validationData, validationLabels, Cgrid)
+
+    def trainAndTune(self, training_data, training_labels, validationData, validation_labels, c_grid):
+        """
+        This method sets self.weights using MIRA.  Train the classifier for each value of C in Cgrid,
+        then store the weights that give the best accuracy on the validationData.
+
+        Use the provided self.weights[label] data structure so that
+        the classify method works correctly. Also, recall that a
+        datum is a counter from features to values for those features
+        representing a vector of values.
+        """
+        best_accuracy = 0
+        final_weights = 0
+
+        for c in c_grid:
+            self.initializeWeightsToZero()
+
+            for iteration in range(self.max_iterations):
+                for x, y in zip(training_data, training_labels):
+                    y_predicted = self.classify([x])[0]
+                    if y_predicted != y:
+                        t = float((((self.weights[y_predicted] - self.weights[y]) * x) + 1)) / (2 * (x * x))
+                        t = min(t, c)
+                        for key in x:
+                            self.weights[y][key] += x[key] * t
+                            self.weights[y_predicted][key] -= x[key] * t
+            y_predicteds = self.classify(validationData)
+            num_correct = 0
+            for y_predicted, validation_label in zip(y_predicteds, validation_labels):
+                num_correct += (y_predicted == validation_label)
+            curent_accuracy = float(num_correct) / len(y_predicteds)
+
+            if curent_accuracy > best_accuracy:
+                best_accuracy = curent_accuracy
+                final_weights = self.weights
+
+        self.weights = final_weights
+
+    def classify(self, data ):
+        """
+        Classifies each datum as the label that most closely matches the prototype vector
+        for that label.  See the project description for details.
+
+        Recall that a datum is a util.counter...
+        """
+        guesses = []
+        for datum in data:
+            vectors = util.Counter()
+            for l in self.legalLabels:
+                vectors[l] = self.weights[l] * datum
+            guesses.append(vectors.argMax())
+        return guesses
+
+
