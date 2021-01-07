7dfa988825a99322655c254fce8c85b7fd6edfa9
Nhật Ngô Sách <nhatngo11a1@gmail.com>
2021-01-08 05:44:24 +0700

    question 1

--- reinforcement/valueIterationAgents.py
+++ reinforcement/valueIterationAgents.py
@@ -16,6 +16,7 @@ import mdp, util
 
 from learningAgents import ValueEstimationAgent
 
+
 class ValueIterationAgent(ValueEstimationAgent):
     """
         * Please read learningAgents.py before reading this.*
@@ -25,7 +26,8 @@ class ValueIterationAgent(ValueEstimationAgent):
         for a given number of iterations using the supplied
         discount factor.
     """
-    def __init__(self, mdp, discount = 0.9, iterations = 100):
+
+    def __init__(self, mdp, discount=0.9, iterations=100):
         """
           Your value iteration agent should take an mdp on
           construction, run the indicated number of iterations
@@ -41,11 +43,26 @@ class ValueIterationAgent(ValueEstimationAgent):
         self.mdp = mdp
         self.discount = discount
         self.iterations = iterations
-        self.values = util.Counter() # A Counter is a dict with default 0
+        self.values = util.Counter()  # A Counter is a dict with default 0
 
         # Write value iteration code here
         "*** YOUR CODE HERE ***"
-
+        # Value iteration loop set up for self.iterations iterations
+        for i in range(iterations):
+            # intializing a temporary counter to store an iteration's value for each state.
+            iterationValues = util.Counter()
+            # looking at each state
+            for s in self.mdp.getStates():
+                # if the state is terminal, the reward is the exit reward and no discounted rewards as it is the
+                # absorbing state
+                if self.mdp.isTerminal(s):
+                    self.values[s] = self.mdp.getReward(s, 'exit', '')
+                # if the state is non-terminal, then finding the best value as the maximum of expected sum of rewards
+                # of different actions.
+                else:
+                    actions = self.mdp.getPossibleActions(s)
+                    iterationValues[s] = max([self.computeQValueFromValues(s, a) for a in actions])
+            self.values = iterationValues
 
     def getValue(self, state):
         """
@@ -53,14 +70,24 @@ class ValueIterationAgent(ValueEstimationAgent):
         """
         return self.values[state]
 
-
     def computeQValueFromValues(self, state, action):
         """
           Compute the Q-value of action in state from the
           value function stored in self.values.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # computing the transitions states and probability
+        transitionStatesAndProps = self.mdp.getTransitionStatesAndProbs(state, action)
+        value = 0
+        # for each transition, the value is calculated as the sum of reward of getting to that transition and
+        # discounted value of transition state summing these transition values gives the q-value for a state action
+        # pair.
+        for ts in transitionStatesAndProps:
+            stateTransitionReward = self.mdp.getReward(state, action, ts[0])
+            value = value + stateTransitionReward + self.discount * (self.values[ts[0]] * ts[1])
+            # print value
+
+        return value
 
     def computeActionFromValues(self, state):
         """
@@ -72,7 +99,13 @@ class ValueIterationAgent(ValueEstimationAgent):
           terminal state, you should return None.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # initializing a stateAction counter, which is used to hold the q-value for each
+        # state action pair. The policy or action is the one that gives the best expected sum of rewards.
+        stateAction = util.Counter()
+        for a in self.mdp.getPossibleActions(state):
+            stateAction[a] = self.computeQValueFromValues(state, a)
+        policy = stateAction.argMax()
+        return policy
 
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
